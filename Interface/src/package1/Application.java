7e1c23eaa5ebfc915135f821a241d5ec17cd8089
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-06 15:47:24 +0200

    correction

--- Interface/src/package1/Application.java
+++ Interface/src/package1/Application.java
@@ -4,13 +4,24 @@ public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		// Création d'une instance pour la note minimale 
 		InplInterface1 noteMinimale = new InplInterface1();
+		// Création d'une instance pour la note maximale
+		InplInterface1 noteMaximale = new InplInterface1();
+	
 		noteMinimale.setV1(14);
 		noteMinimale.setV2(12);
 		
+		noteMaximale.setV1(200);
+		noteMaximale.setV2(1000);
+	
 		
-		System.out.println("La note minimale : " + mot);
+		System.out.println("Voici la note minimale numéro une :" + noteMinimale.getV1());
+		System.out.println("Voici la note minimale numéro deux : " + noteMinimale.getV2());
+		System.out.println("Voici la note minimale : " + noteMinimale.minimum());
+		System.out.println("Voici la note maximale : " + noteMaximale.maximum());
+		
 	}
 
 }
--- Interface/src/package1/ExempleInterface.java
+++ Interface/src/package1/ExempleInterface.java
@@ -1,8 +1,16 @@
 package package1;
 
 public interface ExempleInterface {
-
+	
+	// Signatures des méthodes 
 	public int minimum();
 	public int maximum();
+	public int getV1();
+	public int getV2();
+	
+	// Les signatures contiennent les arguments 
+	public void setV1(int v1);
+	public void setV2(int v2);
+	
 	
 }
